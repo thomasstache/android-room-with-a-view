260b11b73924071698a15fb2a925d877b4eab0f4
Thomas Stache <dev@thomasstache.de>
2020-01-04 23:06:32 +0100

    Step 12 - Populate database on creation

--- app/src/main/java/com/example/android/roomwordssample/WordRoomDatabase.kt
+++ app/src/main/java/com/example/android/roomwordssample/WordRoomDatabase.kt
@@ -4,6 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @Database(entities = [Word::class], version = 1, exportSchema = false)
 abstract class WordRoomDatabase : RoomDatabase() {
@@ -14,7 +18,7 @@ abstract class WordRoomDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: WordRoomDatabase? = null
 
-        fun getDatabase(context: Context): WordRoomDatabase {
+        fun getDatabase(context: Context, scope: CoroutineScope): WordRoomDatabase {
             val instance1 = INSTANCE
             if (instance1 != null) {
                 return instance1
@@ -25,9 +29,37 @@ abstract class WordRoomDatabase : RoomDatabase() {
                     context.applicationContext,
                     WordRoomDatabase::class.java,
                     "word_database"
-                ).build()
+                )
+                    .addCallback(WordDatabaseCallback(scope))
+                    .build()
                 INSTANCE = instance
                 return instance
+            }
+        }
+
+        private class WordDatabaseCallback(private val scope: CoroutineScope) :
+            RoomDatabase.Callback() {
+
+            /**
+             * Override the onOpen method to populate the database.
+             * For this sample, we clear the database every time it is created.
+             */
+            override fun onCreate(db: SupportSQLiteDatabase) {
+                super.onCreate(db)
+                INSTANCE?.let { database ->
+                    scope.launch(Dispatchers.IO) {
+                        populateDatabase(database.wordDao())
+                    }
+                }
+            }
+
+            private suspend fun populateDatabase(wordDao: WordDao) {
+                // delete all content
+                wordDao.deleteAll()
+
+                // add sample words
+                wordDao.insert(Word("Hello"))
+                wordDao.insert(Word("World!"))
             }
         }
     }
