8dd4672cbe948f7d5f04dcad5ad79f9df5cf0dff
Thomas Stache <dev@thomasstache.de>
2020-01-04 21:53:01 +0100

    Step 6..7 - Room database

--- app/src/main/java/com/example/android/roomwordssample/WordDao.kt
+++ app/src/main/java/com/example/android/roomwordssample/WordDao.kt
@@ -1,5 +1,6 @@
 package com.example.android.roomwordssample
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
@@ -9,7 +10,7 @@ import androidx.room.Query
 interface WordDao {
 
     @Query("SELECT * FROM word_table ORDER BY word ASC")
-    fun getAlphabetizedWord(): List<Word>
+    fun getAlphabetizedWords(): LiveData<List<Word>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(word: Word)
---
+++ app/src/main/java/com/example/android/roomwordssample/WordRoomDatabase.kt
@@ -0,0 +1,34 @@
+package com.example.android.roomwordssample
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Word::class], version = 1, exportSchema = false)
+abstract class WordRoomDatabase : RoomDatabase() {
+
+    abstract fun wordDao(): WordDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: WordRoomDatabase? = null
+
+        fun getDatabase(context: Context): WordRoomDatabase {
+            val instance1 = INSTANCE
+            if (instance1 != null) {
+                return instance1
+            }
+
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    WordRoomDatabase::class.java,
+                    "word_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
