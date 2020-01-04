639c42b4e529772f519032b83ed8a93af35cb36b
Thomas Stache <dev@thomasstache.de>
2020-01-04 22:15:47 +0100

    Step 8..9 - Data layer: ViewModel and Repository - the viewModel has application scope

---
+++ app/src/main/java/com/example/android/roomwordssample/WordViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.android.roomwordssample
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class WordViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository: WordRepository
+
+    val allWords: LiveData<List<Word>>
+
+    init {
+        // Gets reference to WordDao from WordRoomDatabase to construct
+        // the correct WordRepository
+        val wordsDao = WordRoomDatabase.getDatabase(application).wordDao()
+        repository = WordRepository(wordsDao)
+        allWords = repository.allWords
+    }
+
+    /**
+     * The implementation of insert() in the database is completely hidden from the UI.
+     * Room ensures that you're not doing any long running operations on
+     * the main thread, blocking the UI, so we don't need to handle changing Dispatchers.
+     * ViewModels have a coroutine scope based on their lifecycle called
+     * viewModelScope which we can use here.
+     */
+    fun insert(word: Word) = viewModelScope.launch {
+        repository.insert(word)
+    }
+}
