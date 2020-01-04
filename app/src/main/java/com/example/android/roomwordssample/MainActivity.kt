14f900a0b1b9d830f076bc0e211548fbff3daf8a
Thomas Stache <dev@thomasstache.de>
2020-01-04 22:41:59 +0100

    Step 11 - RecyclerView with Adapter and ViewHolder

--- app/src/main/java/com/example/android/roomwordssample/MainActivity.kt
+++ app/src/main/java/com/example/android/roomwordssample/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.android.roomwordssample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
+        recyclerview.adapter = WordListAdapter(this)
+        recyclerview.layoutManager = LinearLayoutManager(this)
     }
 }
