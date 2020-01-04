2942708b01753b1c1d5bf77f6f9e07c92b255e3b
Thomas Stache <dev@thomasstache.de>
2020-01-04 23:26:03 +0100

    Step 14a - Display the data from the ViewModel

--- app/src/main/java/com/example/android/roomwordssample/MainActivity.kt
+++ app/src/main/java/com/example/android/roomwordssample/MainActivity.kt
@@ -2,17 +2,34 @@ package com.example.android.roomwordssample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var wordViewModel: WordViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
-        recyclerview.adapter = WordListAdapter(this)
+        val adapter = WordListAdapter(this)
+        recyclerview.adapter = adapter
         recyclerview.layoutManager = LinearLayoutManager(this)
+
+        // Get a new or existing ViewModel from the ViewModelProvider.
+        wordViewModel = ViewModelProviders.of(this).get(WordViewModel::class.java)
+
+        // Add an observer on the LiveData returned by getAlphabetizedWords.
+        // The onChanged() method fires when the observed data changes and the activity is
+        // in the foreground.
+        wordViewModel.allWords.observe(this, Observer { words ->
+            // Update the cached copy of the words in the adapter.
+            words?.let { adapter.setWords(it) }
+        })
+
     }
 }
